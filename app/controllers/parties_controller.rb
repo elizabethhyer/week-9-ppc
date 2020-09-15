e8f1bc5bb7305207be73a60227fcbf81e2c34b59
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-09-15 16:07:18 -0400

    Adds new form and new + create methods in controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -7,5 +7,23 @@ class PartiesController < ApplicationController
       @party = Party.find(params[:id])
   end
 
+  def new
+    @party = Party.new
+  end 
 
+  def create 
+    @party = Party.new(party_params(:name, :date, :budget)) 
+    if @party.valid?
+      @party.save
+      redirect_to party_path(@party)
+    else 
+      render :new 
+    end 
+  end 
+
+  private 
+
+  def party_params(*args)
+    params.require(:party).permit(args) 
+  end 
 end
